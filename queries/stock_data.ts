49bf10d638a1316f7e80602f36cabd4c4c74cf15
Julian Elmasry <julian31186@gmail.com>
2022-01-31 23:12:41 -0500

    exporting function

--- queries/stock_data.ts
+++ queries/stock_data.ts
@@ -3,7 +3,6 @@ var request = require('request');
 import dotenv from 'dotenv'
 dotenv.config()
 
-ticker('AMD');
 
 function ticker(tickerSymbol:String) {
   var url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${tickerSymbol}&apikey=${process.env.API_KEY}`;
@@ -15,17 +14,15 @@ function ticker(tickerSymbol:String) {
   }, (err:Error, res: any, data:Object) => {
     if (err) {
       console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
+    } else if (Object.keys(data).length === 0) {
+     return 0;
     } else {
       // data is successfully parsed as a JSON object:
       console.log(data);
     }
   });
 
-
-  console.log(url);
 }
 
 
-
+export default ticker;
